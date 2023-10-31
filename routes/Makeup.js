f6e10477cbbe4c85944022f4446d902f6df69700
S567423 <S567423@nwmissouri.edu>
2023-10-31 00:24:42 -0500

    updated Makeup

--- routes/Makeup.js
+++ routes/Makeup.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Makeup', { title: 'Search'});
+  res.render('Makeup', { title: 'Search Results Makeup'});
 });
 
 module.exports = router;
