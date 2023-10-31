4d182be9e30ef5f1bcc41e9bf61c6f4493c14126
S567423 <S567423@nwmissouri.edu>
2023-10-31 00:17:53 -0500

    Added Makeup endpoint

--- routes/Makeup.js
+++ routes/Makeup.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Makeup', { title: 'Search Results Makeup' });
+  res.render('Makeup', { title: 'Search' });
 });
 
 module.exports = router;
