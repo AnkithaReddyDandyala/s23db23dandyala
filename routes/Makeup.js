7176ca5d1ed610aa96b140be53299637057869d9
S567423 <S567423@nwmissouri.edu>
2023-11-15 22:32:14 -0600

    Updated makeup.js controller

--- routes/Makeup.js
+++ routes/Makeup.js
@@ -4,5 +4,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', makeup_controller.makeup_view_all_Page );
-router.get('/makeup/:id', makeup_controller.makeup_detail);
+/* GET detail vehicles page */
+router.get('/detail', makeup_controller.makeup_view_one_Page);
+/* GET create vehicle page */
+router.get('/create', makeup_controller.makeup_create_Page);
+/* GET create update page */
+router.get('/update', makeup_controller.makeup_update_Page);
+/* GET delete costume page */
+router.get('/delete', makeup_controller.makeup_delete_Page);
 module.exports = router;
